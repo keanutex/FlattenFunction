ab185e9f2bed402349d0f9bc8d9d80ff9f5cea77
Keanutex <keanutex@gmail.com>
2022-07-06 11:31:55 +0200

    Fixed edge cases and added tests for edge cases

--- flatten.js
+++ flatten.js
@@ -1,7 +1,8 @@
 function flatten(arr) {
   let res = [];
+  let arrLength = arr ? arr.length : 0;
 
-  for (let i = 0; i < arr.length; i++) {
+  for (let i = 0; i < arrLength; i++) {
     if (Array.isArray(arr[i])) {
       res = res.concat(flatten(arr[i]));
     } else {
@@ -17,3 +18,38 @@ const assert = require("assert");
 
 let onlyNumbersNestedArray = [1, [2], [2, [3, [5]]], 7, [8]];
 assert.deepEqual(flatten(onlyNumbersNestedArray), [1, 2, 2, 3, 5, 7, 8]);
+
+let mixedTypeNestedArray = [
+  "b",
+  3,
+  undefined,
+  [[{ a: "a" }]],
+  [null],
+  "a",
+  [[[2]], null, "hello"],
+];
+assert.deepEqual(flatten(mixedTypeNestedArray), [
+  "b",
+  3,
+  undefined,
+  {
+    a: "a",
+  },
+  null,
+  "a",
+  2,
+  null,
+  "hello",
+]);
+
+let emptyArray = [];
+assert.deepEqual(flatten(emptyArray), []);
+
+let notAnArray = 5;
+assert.deepEqual(flatten(notAnArray), []);
+
+assert.deepEqual(flatten(undefined), []);
+
+assert.deepEqual(flatten(null), []);
+
+//Testing End
